07a7b6faf350b147da5cdd1bd8e3424a7f60f6c3
Alexandru Gurita <alexandru.gurita@flipsidegroup.com>
2022-09-07 17:44:00 +0300

    Fixed api url

--- js/utils/constants/generalConstants.ts
+++ js/utils/constants/generalConstants.ts
@@ -3,8 +3,8 @@ import placeholderImage from '../../../public/PlaceholderImage.png';
 const PLACEHOLDER_IMAGE =  placeholderImage;
 const PLACEHOLDER_IMAGE_ALT = 'Placeholder image';
 
-const WEB_URL = process.env.ENVIRONMENT !== 'development' ? 'http://localhost:3000' : 'https://glimpseofromania.com';
-const API_URL = process.env.ENVIRONMENT !== 'development' ? 'http://localhost:1337' : 'https://cms-glimpseofromania.com';
+const WEB_URL = process.env.ENVIRONMENT === 'development' ? 'http://localhost:3000' : 'https://glimpseofromania.com';
+const API_URL = process.env.ENVIRONMENT === 'development' ? 'http://localhost:1337' : 'https://cms-glimpseofromania.com';
 
 const INSTAGRAM_LINK = 'https://www.instagram.com/';
 const FACEBOOK_LINK = 'https://www.facebook.com/';
